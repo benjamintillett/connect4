a757b2ba1397d8df19b0a6e5a92d8e8f6d5485d0
benjamin tillett <benjamin.tillett@gmail.com>
2015-08-19 11:10:47 +0100

    refoactor remove cell

--- apiTests/connect4Spec.js
+++ apiTests/connect4Spec.js
@@ -11,7 +11,7 @@ describe('connect4',function(){
 			connect4.initializeBoard(3,2);
 			expect(connect4.board.length).to.eql(3)
 			expect(connect4.board[0].length).to.eql(2)
-			expect(connect4.board[0][0].constructor).to.eql(Cell)
+			expect(connect4.board[0][0]).to.eql(' ')
 		});		
 	});
 
@@ -28,22 +28,79 @@ describe('connect4',function(){
 
 		it("doesn't allow a move in a full column",function(){
 			connect4.board = [
-						 [new Cell(' '),new Cell('x')],
-						 [new Cell('x'),new Cell('o')],
-						 [new Cell('x'),new Cell('x')]
+						 [' ','x'],
+						 ['x','o'],
+						 ['x','x']
 						]
 			expect(connect4.makeMove(1,"x")).to.eql(false);
 		});
 
 		it("a valid move adds the player chip to the correct cell cell marked",function(){
 			connect4.board = [
-						 [new Cell(' '),new Cell(' ')],
-						 [new Cell('x'),new Cell('o')],
-						 [new Cell('x'),new Cell('x')]
+						 [' ',' '],
+						 ['x','o'],
+						 ['x','x']
 						]
 			connect4.makeMove(1,"x")
-			expect(connect4.board[0][1].content).to.eql('x');
+			expect(connect4.board[0][1]).to.eql('x');
 		});
+	});
+
+	describe('checkForVictory', function(){
+		var board;
+
+		before(function(){
+			connect4.rows = 5;
+			connect4.columns = 5;
+			connect4.board = [
+					 [' ',' ',' ',' ',' '],
+					 [' ',' ',' ',' ','o'],
+					 [' ',' ',' ',' ','o'],
+					 [' ',' ',' ',' ','o'],
+					 ['x','x','x',' ','o'],
+					]
+		});
+
+		it('knows x has not won',function(){
+			expect(connect4.checkForVictory('x')).to.eql(false);
+		});
+
+		it('can recognize a vertical win', function(){
+			connect4.board = [
+					 [' ',' ',' ',' ',' '],
+					 [' ',' ',' ',' ','o'],
+					 [' ',' ',' ',' ','o'],
+					 [' ',' ',' ',' ','o'],
+					 ['x','x','x',' ','o'],
+			]
+			connect4.rows = 5;
+			connect4.columns = 5;
+			expect(connect4.checkForVictory('o')).to.eql(true);
+		});
+
+		// it('can recognize a horizontal win',function(){
+		// 	connect4.board = [
+		// 			 [' ',' ',' ',' ',' '],
+		// 			 [' ',' ',' ',' ',' '],
+		// 			 [' ',' ',' ',' ',' '],
+		// 			 [' ',' ',' ',' ',' '],
+		// 			 ['x','x','x','x',' '],
+		// 			]
+		// 	connect4.rows = 5;
+		// 	connect4.columns = 5;
+		// 	expect(connect4.checkForVictory('x')).to.eql(true);
+		// });
+		// it('can recognize a upwards digonal win',function(){
+		// 	board = [
+		// 			 [' ',' ',' ',' ',' '],
+		// 			 [' ',' ',' ','o',' '],
+		// 			 [' ',' ','o','x',' '],
+		// 			 ['x','o','o','o',' '],
+		// 			 ['o','x','x','x',' '],
+		// 			]
+		// 	var player = "x";
+		// 	expect(connect4.checkForVictory(player,3,board)).to.eql(true);
+		// });
 	});
 
 });
--- app/lib/connect4.js
+++ app/lib/connect4.js
@@ -1,4 +1,4 @@
-Cell = require('./Cell');
+
 
 function Connect4(){
 	this.initializeBoard = function(rows,columns){
@@ -6,40 +6,141 @@ function Connect4(){
 		for(var i = 0; i < rows; i++){
 			var row = [];
 			for(var j = 0; j < columns; j++){
-				row.push(new Cell(" "));
+				row.push(" ");
 			}
 			board.push(row);
 		}
-		this.board = board;
+		this.rows 	= rows;
+		this.columns = columns; 
+		this.board 	= board;
 	}	
 	this.makeMove = function(column,player){
-		if (column < 0 || column >= this.board.length) return false;
+		if (!this.isInBoard({ column: column})) return false;
 		if (this.columnIsFull(column,this.board)) return false;		
 		this.placeChip(column,player)
 		return this.board
 	}
 
-
+	this.isInBoard = function(coordinates){
+		var row = coordinates.row || 0;
+		var column = coordinates.column;
+		return (this.board[0][column] != undefined && this.board[row] != undefined && this.board[row][column] != undefined)
+	}
 
 	this.placeChip = function(column,player){
 		var emptyRow;
 		this.board.forEach(function(row,index){
-			if (row[column].content == ' ') emptyRow = index
+			if (row[column] == ' ') emptyRow = index
 		});
-		this.board[emptyRow][column].content = player;
+		this.board[emptyRow][column] = player;
 		return this.board;	
 	}
 
 	this.columnIsFull = function(column){
 		var isFull = true
 		this.board.forEach(function(row){
-			if(row[column].content == ' '){
+			if(row[column]== ' '){
 				isFull = false;
 			}
 		});
 		return isFull;
 	}
 
+	this.checkForVictory = function(player){
+		if(this.hasVerticalConnect4(player)) return true
+		if(this.hasHorizontalConnect4(player)) return true
+		return false
+	}
+
+	this.hasVerticalConnect4 = function(player){
+		var columns = this.columns;
+		var rows 	= this.rows;
+		var board 	= this.board;
+		for(var column = 0; column < columns; column++){
+			var count = 0
+			var cellValue;
+			for(var row = 0; row < rows; row++){
+				cellValue 		= broad[column];
+				if (cellValue == player){
+					count++
+					if(count >= 4) {
+						lineOf4 = true;
+					}
+				} else {
+					count = 0;
+				}
+				
+			});	
+		}
+		return lineOf4;
+	} 
+
+	this.hasVerticalConnect4 = function(player){
+		var lineOf4 = false
+		for(var column = 0; column < this.columns; column++){
+			var count = 0
+			var cellValue;
+			this.board.forEach(function(row){
+				cellValue 		= row[column];
+				if (cellValue == player){
+					count++
+					if(count >= 4) {
+						lineOf4 = true;
+					}
+				} else {
+					count = 0;
+				}
+				
+			});	
+		}
+		return false;
+	} 
+
+	this.hasHorizontalConnect4 = function(player){
+		var columns = this.columns;
+		var lineOf4 = false
+		this.board.forEach(function(row){
+			var cellValue;
+			var count = 0
+			for(var column = 0; column < columns; column++){
+				cellValue 		= row[column];
+				if (cellValue == player){
+					count++
+					if(count >= 4) {
+						lineOf4 = true;
+					}
+				} else {
+					count = 0;
+				}
+			}
+		});
+		return lineOf4;
+	}
+	this.hasDiagonallyUpConnect4 = function(player){
+		var columns = this.columns;
+		var lineOf4 = false
+		this.board.forEach(function(row){
+			var cellValue;
+			var count = 0
+			for(var column = 0; column < columns; column++){
+				cellValue 		= row[column];
+				if (cellValue == player){
+					count++
+					if(count >= 4) {
+						lineOf4 = true;
+					}
+				} else {
+					count = 0;
+				}
+			}
+		});
+		return lineOf4;
+	}
+
 }
 
 module.exports = Connect4;
+
+
+
+
